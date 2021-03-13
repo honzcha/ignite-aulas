f4e0b3a7373958dd2ec5dbf8fa0be46485870b68
Lucas Garcia <honzcha@outlook.com>
2021-03-13 21:25:44 +0800

    mar-11-2021

---
+++ 01_react/01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,29 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Repo List</h1>
+      <ul>
+        <li>
+          <strong>Repo Name</strong>
+          <p>Repo Description</p>
+          <a href="">Access the Repo</a>
+        </li>{" "}
+        <li>
+          <strong>Repo Name</strong>
+          <p>Repo Description</p>
+          <a href="">Access the Repo</a>
+        </li>{" "}
+        <li>
+          <strong>Repo Name</strong>
+          <p>Repo Description</p>
+          <a href="">Access the Repo</a>
+        </li>{" "}
+        <li>
+          <strong>Repo Name</strong>
+          <p>Repo Description</p>
+          <a href="">Access the Repo</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
