c27f211896253a8ba69d131aec17579a75244c29
Lucas Garcia <honzcha@outlook.com>
2021-03-25 21:26:21 +0800

    mar-23-2021

--- 02_first_react/money/src/App.tsx
+++ 02_first_react/money/src/App.tsx
@@ -1,14 +1,11 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6
-`
+import React from "react";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-      <Title>Hello You</Title>
+      <h1>Hello You</h1>
+      <GlobalStyle/>
     </div>
   );
 }
