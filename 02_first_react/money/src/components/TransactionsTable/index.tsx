5f1888ed3af8c5af867d8ab420e4caa4e9bbd642
lucas <lucas@pop-os.localdomain>
2021-03-29 22:51:02 +0800

    29-mar-2021

--- 02_first_react/money/src/components/TransactionsTable/index.tsx
+++ 02_first_react/money/src/components/TransactionsTable/index.tsx
@@ -1,11 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
   useEffect(() => {
     api.get('transactions')
-      .then(response =>console.log(response.data))
+      .then(response =>setTransactions(response.data.transactions))
   }, []);
 
   return(
@@ -21,18 +32,21 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Task made money</td>
-            <td className="deposit">$1,000.00</td>
-            <td>Category</td>
-            <td>30/03/2021</td>
-          </tr>
-          <tr>  
-            <td>Task lost money</td>
-            <td className="withdraw">-$1,000.00</td>
-            <td>Category</td>
-            <td>30/03/2021</td>
-          </tr>          
+          {transactions.map(transaction =>(
+             <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD'
+                  }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat('en-US').format(new Date(transaction.createdAt))}
+              </td>
+            </tr>
+          ))}      
         </tbody>
 
       </table>
--- 02_first_react/money/src/index.tsx
+++ 02_first_react/money/src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "First income on",
+          type: "income",
+          category: "Dev",
+          amount: 5000,
+          createdAt: new Date('2021-03-10 09:30:00')
+        },
+        {
+          id: 2,
+          title: "Office rent",
+          type: "outcome",
+          category: "Rent",
+          amount: 2000,
+          createdAt: new Date('2021-02-20 14:30:00')
+        }
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
