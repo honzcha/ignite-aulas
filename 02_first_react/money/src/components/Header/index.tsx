216aef0fbf0b580c48b42d3f5b3e8d1361b87140
Lucas Garcia <honzcha@outlook.com>
2021-03-30 20:21:27 +0800

    29-mar-2021

--- 02_first_react/money/src/components/Header/index.tsx
+++ 02_first_react/money/src/components/Header/index.tsx
@@ -13,7 +13,7 @@ export function Header ({onOpenNewTransactionModal}: HeaderProps) {
 			<Content>
 				<img src={logoImg} alt="money"/>
 				<button type="button" onClick={onOpenNewTransactionModal}>
-					New transactions
+					New transaction
 				</button>
 
 
