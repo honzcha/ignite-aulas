61de58200e835b187aab5fd30d923a7f5f1d6d61
Lucas <honzcha@outlook.com>
2021-05-16 18:32:46 +0800

    may 15 2021

---
+++ 03_Next/01_intro/not-substack/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | Not substack</title>
+      </Head>
+        <h1>
+          Hi
+        </h1>
+    </>
+  )
+}
