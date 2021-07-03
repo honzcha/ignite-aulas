4648b8919939323bb107869a5880022975074972
Lucas Garcia <45900433+honzcha@users.noreply.github.com>
2021-07-03 09:44:37 +0800

    2-jun-2021

--- 03_Next/01_intro/not-substack/src/pages/api/auth/[...nextauth].ts
+++ 03_Next/01_intro/not-substack/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -9,5 +13,52 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  // jwt: {
+  //   signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
+  // },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          {data: { email }}
+        )
+      )
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+
+      } catch(err) {
+        console.log(err)
+        return false
+      }
+    },
+  }
 
 })
