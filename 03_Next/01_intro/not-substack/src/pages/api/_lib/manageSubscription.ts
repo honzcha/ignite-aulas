a030533db05c161163ea4ed1422a2915544e4d0b
Lucas Garcia <45900433+honzcha@users.noreply.github.com>
2021-07-04 00:02:21 +0800

    3-jun-2021

--- 03_Next/01_intro/not-substack/src/pages/api/_lib/manageSubscription.ts
+++ 03_Next/01_intro/not-substack/src/pages/api/_lib/manageSubscription.ts
@@ -6,6 +6,7 @@ import { stripe } from '../../../services/stripe'
 export async function saveSubscription(
   subscriptionId: string,
   customerId: string,
+  createAction = false,
 ) {
   // user_by_stripe_customer_id
    const userRef = await fauna.query(
@@ -29,10 +30,27 @@ export async function saveSubscription(
     price_id: subscription.items.data[0].price.id,
   }
 
-   await fauna.query(
-     q.Create(
-       q.Collection('subscriptions'),
-       {data: subscriptionData}
+  if (createAction) {
+    await fauna.query(
+      q.Create(
+        q.Collection('subscriptions'),
+        {data: subscriptionData}
+     )
     )
-   )
+  } else {
+    await fauna.query(
+      q.Replace(
+        q.Select(
+          "ref",
+          q.Get(
+            q.Match(
+              q.Index('subscription_by_id'),
+              subscriptionId,
+            )
+          )
+        ),
+        {data:subscriptionData}
+      )
+    )
+  }
 }
