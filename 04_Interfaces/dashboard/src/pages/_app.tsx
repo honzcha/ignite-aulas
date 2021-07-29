96cde39a7167c76436304c404fce0abec5402a76
Lucas Garcia <lucas_h_garcia@hotmail.com>
2021-07-29 21:29:35 +0800

    -

--- 04_Interfaces/dashboard/src/pages/_app.tsx
+++ 04_Interfaces/dashboard/src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import { AppProps } from 'next/app'
+// chakraprovider will allow to import the theme and apply the theme to the application
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
