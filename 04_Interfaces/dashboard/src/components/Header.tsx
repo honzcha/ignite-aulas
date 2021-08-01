190d02b2ea525363ba683905f4ed2901ea99036c
Lucas Garcia <lucas_h_garcia@hotmail.com>
2021-08-01 23:15:38 +0800

    -

--- 04_Interfaces/dashboard/src/components/Header.tsx
+++ 04_Interfaces/dashboard/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Icon, Input, Text } from "@chakra-ui/react";
-import { RiSearchLine } from 'react-icons/ri'
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -14,7 +14,7 @@ export function Header() {
       align="center"
     >
       <Text
-        fontsize="3xl"
+        fontSize="3xl"
         fontWeight="bold"
         letterSpacing="tight"
         w="64"
@@ -49,6 +49,45 @@ export function Header() {
 
       </Flex>
 
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack 
+          spacing="4"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+        <Icon as={RiNotificationLine} fontSize="20" />
+        <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center" >
+          <Box
+            mr="4"
+            textAlign="right"
+          >
+            <Text>Honzcha</Text>
+            <Text
+              color="gray.300"
+              fontSize="small"
+            >
+              honzcha@email.com
+            </Text>
+          </Box>
+
+          <Avatar 
+            size="md" 
+            name="Honzcha" 
+            src="https://github.com/honzcha.png"
+          />
+
+        </Flex>
+      </Flex>
     </Flex>
   )
 }
