58dfdaf38d517e16cd4eeda6569b44a6e52a79cc
Lucas Garcia <lucas_h_garcia@hotmail.com>
2021-08-08 22:57:48 +0800

    Responsive sidebar

--- 04_Interfaces/dashboard/src/pages/_app.tsx
+++ 04_Interfaces/dashboard/src/pages/_app.tsx
@@ -2,11 +2,14 @@ import { AppProps } from 'next/app'
 // chakraprovider will allow to import the theme and apply the theme to the application
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
+import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <SidebarDrawerProvider>
+        <Component {...pageProps} />
+      </SidebarDrawerProvider>
     </ChakraProvider>
   )
 }
